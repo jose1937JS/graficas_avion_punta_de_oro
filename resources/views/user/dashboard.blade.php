98c6de7359a8e20854d08270714a02a48074a880
josepher <jflo1937@gmail.com>
2019-02-05 15:18:49 -0400

    cositas

--- resources/views/user/dashboard.blade.php
+++ resources/views/user/dashboard.blade.php
@@ -56,7 +56,7 @@
 			</div>
 		</div>
 
-		{{-- <div class="demo-cards mdl-cell mdl-cell--4-col mdl-cell---col-tablet mdl-grid mdl-grid--no-spacing">
+		<div class="demo-cards mdl-cell mdl-cell--4-col mdl-cell---col-tablet mdl-grid mdl-grid--no-spacing">
 			<div class="demo-updates mdl-card mdl-shadow--2dp mdl-cell mdl-cell--4-col mdl-cell--4-col-tablet mdl-cell--12-col-desktop">
 				<div class="mdl-card__title mdl-card--expand mdl-color--teal-300">
 					<h2 class="mdl-card__title-text">Updates</h2>
@@ -105,7 +105,7 @@
 					<i class="material-icons">location_on</i>
 				</div>
 			</div>
-		</div> --}}
+		</div>
 	</div>
 @stop
 
